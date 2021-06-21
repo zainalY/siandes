712d35fa13fd0380992ef36082861ad197410771
zainalY <50313022+zainalY@users.noreply.github.com>
2021-06-21 21:42:20 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,151 @@
+$(window).on("load", function () {
+    /*--------------Preloader--------------*/
+    $(".preloader").fadeOut("slow");
+});
+
+$(document).ready(function () {
+    /*--------------Navbar Shrink--------------*/
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > 90) {
+            $(".navbar").addClass("navbar-shrink");
+        } else {
+            $(".navbar").removeClass("navbar-shrink");
+        }
+    });
+
+    /*----------------Video Popup----------------*/
+    const videoSrc = $("#player-1").attr("src");
+    $(".video-play-btn, .video-popup").on("click", function () {
+        if ($(".video-popup").hasClass("open")) {
+            $(".video-popup").removeClass("open");
+            $("#player-1").attr("src", "");
+        }
+        else {
+            $(".video-popup").addClass("open");
+            if ($("#player-1").attr("src") == '') {
+                $("#player-1").attr("src", videoSrc);
+            }
+        }
+
+    });
+
+    /*----------------Features Carousel----------------*/
+    $('.features-caraousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        autoplay: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 3,
+            }
+        }
+    })
+
+    /*----------------Screenshots Carousel----------------*/
+    $('.screenshots-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        autoplay: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 4,
+            }
+        }
+    })
+
+    /*----------------Testimonials Carousel----------------*/
+    $('.testimonials-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        autoplay: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 3,
+            }
+        }
+    })
+
+    /*----------------Team Carousel----------------*/
+    $('.team-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        autoplay: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 3,
+            }
+        }
+    })
+
+    /*----------------Page Scrolling - ScrollIt----------------*/
+    $.scrollIt({
+        topOffset: -50
+    });
+
+    /*----------------Navbar Collapse----------------*/
+    $(".nav-link").on("click", function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+    /*----------------Navbar Collapse----------------*/
+    function toggleTheme() {
+        if (localStorage.getItem("simdes-theme") !== null) {
+            if (localStorage.getItem("simdes-theme") == "dark") {
+                $("body").addClass("dark");
+            } else {
+                $("body").removeClass("dark");
+            }
+        }
+        updateIcon();
+    }
+
+    toggleTheme();
+
+    $(".toggle-theme i").on("click", function () {
+        $("body").toggleClass("dark");
+        if ($("body").hasClass("dark")) {
+            localStorage.setItem("simdes-theme", "dark");
+        } else {
+            localStorage.setItem("simdes-theme", "light");
+        }
+        updateIcon();
+    });
+
+    function updateIcon() {
+        if ($("body").hasClass("dark")) {
+            $(".toggle-theme i").removeClass("fa-moon");
+            $(".toggle-theme i").addClass("fa-sun");
+        } else {
+            $(".toggle-theme i").removeClass("fa-sun");
+            $(".toggle-theme i").addClass("fa-moon");
+        }
+    }
+});
